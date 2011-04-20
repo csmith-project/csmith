90c262926b9418426c6e131de5ff24985bd094a1
John Regehr <regehr@cs.utah.edu>
2011-04-20 15:51:39 +0000

    be a bit more verbose

--- utah/scripts/reduce_log.sh
+++ utah/scripts/reduce_log.sh
@@ -1,7 +1,11 @@
-${CSMITH_PATH}/utah/scripts/reduce.sh > delta.log
+echo > delta.log
+
+date >> delta.log
+${CSMITH_PATH}/utah/scripts/reduce.sh >> delta.log
 set -v
 grep -c here1 delta.log >> delta.log
 grep -c here2 delta.log >> delta.log
 grep -c here3 delta.log >> delta.log
 grep -c here4 delta.log >> delta.log
 grep -c here5 delta.log >> delta.log
+date >> delta.log
