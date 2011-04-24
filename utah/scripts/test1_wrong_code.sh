3468b744a57528f7442d7831a66bc5d2bdb3f434
John Regehr <regehr@cs.utah.edu>
2011-04-24 03:37:46 +0000

    minor script work

--- utah/scripts/test1_wrong_code.sh
+++ utah/scripts/test1_wrong_code.sh
@@ -9,20 +9,23 @@ rm -f out*.txt
 
 if 
   echo here1 &&\
-  current-gcc -Wall -O small.c  >outa.txt 2>&1 &&\
+  current-gcc -Wall -Wextra -O small.c  >outa.txt 2>&1 &&\
   echo here2 &&\
   ! grep uninitialized outa.txt &&\
   ! grep 'control reaches end' outa.txt &&\
+  ! grep 'ordered comparison of pointer with integer' outa.txt &&\
   ! grep 'incompatible implicit' outa.txt &&\
   ! grep 'comparison between pointer and integer' outa.txt &&\
   clang -pedantic -Wall -O0 -c small.c  >out.txt 2>&1 &&\
   ! grep 'incompatible redeclaration' out.txt &&\
+  ! grep 'ordered comparison between pointer' out.txt &&\
   ! grep 'eliding middle term' out.txt &&\
   ! grep 'end of non-void function' out.txt &&\
   ! grep 'invalid in C99' out.txt &&\
   ! grep 'should return a value' out.txt &&\
   ! grep uninitialized out.txt &&\
-  ! grep 'incompatible pointer' out.txt &&\
+  ! grep 'incompatible pointer to' out.txt &&\
+  ! grep 'incompatible integer to' out.txt &&\
   ! grep 'type specifier missing' out.txt &&\
   echo here3 &&\
   $CC1 small.c -o small1 > cc_out1.txt 2>&1 &&\
