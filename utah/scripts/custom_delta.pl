bf46df70e9c6c9d186e99abc2b7d283175089dcc
John Regehr <regehr@cs.utah.edu>
2011-04-28 16:13:20 +0000

    minor change

--- utah/scripts/custom_delta.pl
+++ utah/scripts/custom_delta.pl
@@ -3,6 +3,7 @@
 use strict;
 
 # easy
+#   also make replace with 0, 1, nothing include the suffix
 #   remove bitfield specifiers like :3
 #   turn short, char, long into int
 #   replace complex assignments like ^= with =
@@ -141,7 +142,7 @@ sub write_file ($)
 my $num = "\\-?[xX0-9a-fA-F]+[UL]*";
 my $var1a = "(\&*\\**)[lgpt]_[0-9]+(\\\[$num\\\])*";
 my $var1b = "(\&*\\**)[lgpt]_[0-9]+(\\.f[0-9]+)*";
-my $var2 = "si1|si2|ui1|ui2|func_([0-9]+)";
+my $var2 = "si1|si2|ui1|ui2|left|right|val|crc32_context|func_([0-9]+)";
 my $var = "($var1a)|($var1b)|($var2)";
 my $arith = "\\+|\\-|\\%|\\/|\\*";
 my $comp = "\\<\\=|\\>\\=|\\<|\\>|\\=\\=|\\!\\=|\\=";
