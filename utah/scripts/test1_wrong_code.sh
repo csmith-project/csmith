3e3668f0bda3668758a09e62217b2feace1035f5
John Regehr <regehr@cs.utah.edu>
2011-05-06 16:42:03 -0600

    Check for a few more compiler warnings.

--- utah/scripts/test1_wrong_code.sh
+++ utah/scripts/test1_wrong_code.sh
@@ -13,8 +13,10 @@ if
   echo here2 &&\
   ! grep uninitialized outa.txt &&\
   ! grep 'control reaches end' outa.txt &&\
+  ! grep 'no semicolon at end' outa.txt &&\
   ! grep 'incompatible pointer' outa.txt &&\
   ! grep 'ordered comparison of pointer with integer' outa.txt &&\
+  ! grep 'pointer from integer' outa.txt &&\
   ! grep 'incompatible implicit' outa.txt &&\
   ! grep 'excess elements in struct initializer' outa.txt &&\
   ! grep 'comparison between pointer and integer' outa.txt &&\
