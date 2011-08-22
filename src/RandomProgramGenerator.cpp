b91175012c690682831081a23066aa5ed1000d31
John Regehr <regehr@cs.utah.edu>
2011-08-22 11:34:01 -0600

    Oops, also add this option to usage info.  Now I see that I failed to add a "yes safe math" option.  Maybe someone else can fix...

--- src/RandomProgramGenerator.cpp
+++ src/RandomProgramGenerator.cpp
@@ -175,6 +175,7 @@ static void print_help()
 	cout << "  --max-union-fields <num>: limit the number of union fields to <num> (default 5). " << endl << endl;
 
 	cout << "  --muls | --no-muls: enable | disable multiplications (enabled by default)." << endl << endl;
+	cout << "  --no-safe-math: Do not emit safe math wrapper functions." << endl << endl;
 	cout << "  --packed-struct | --no-packed-struct: enable | disable packed structs by adding #pragma pack(1) before struct definition (disabled by default)." << endl << endl; 
 	cout << "  --paranoid | --no-paranoid: enable | disable pointer-related assertions (disabled by default)." << endl << endl; 
 	cout << "  --pointers | --no-pointers: enable | disable pointers (enabled by default)." << endl << endl;
