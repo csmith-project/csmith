462bf4111cf8c4df110f278f5d435c558d414eac
John Regehr <regehr@cs.utah.edu>
2011-11-07 21:40:21 -0700

    Test scripts from the paper worked really well, let's add them to Csmith.

--- utah/scripts/reduce/test1_crash.sh
+++ utah/scripts/reduce/test1_crash.sh
@@ -1,47 +1,40 @@
 #!/bin/bash
 
-# todo: these come from environment variables
-
-CC1='current-gcc -Ofast -funroll-loops -fomit-frame-pointer -fwhole-program'
-CRASHSTR='eliminate_redundant_comparison'
-
 rm -f out*.txt
 
 if 
-  echo here1 &&\
-  current-gcc -Wall -O small.c  >outa.txt 2>&1 &&\
-  echo here2 &&\
+  gcc -c -Wall -Wextra -O2 small.c  >outa.txt 2>&1 &&\
   ! grep uninitialized outa.txt &&\
   ! grep 'control reaches end' outa.txt &&\
+  ! grep 'no semicolon at end' outa.txt &&\
+  ! grep 'incompatible pointer' outa.txt &&\
+  ! grep 'ordered comparison of pointer with integer' outa.txt &&\
+  ! grep 'declaration does not declare anything' outa.txt &&\
+  ! grep 'expects type' outa.txt &&\
+  ! grep 'pointer from integer' outa.txt &&\
   ! grep 'incompatible implicit' outa.txt &&\
+  ! grep 'excess elements in struct initializer' outa.txt &&\
   ! grep 'comparison between pointer and integer' outa.txt &&\
   clang -pedantic -Wall -O0 -c small.c  >out.txt 2>&1 &&\
   ! grep 'incompatible redeclaration' out.txt &&\
+  ! grep 'ordered comparison between pointer' out.txt &&\
   ! grep 'eliding middle term' out.txt &&\
   ! grep 'end of non-void function' out.txt &&\
   ! grep 'invalid in C99' out.txt &&\
+  ! grep 'specifies type' out.txt &&\
   ! grep 'should return a value' out.txt &&\
-  ! grep uninitialized out.txt &&\
-  ! grep 'incompatible pointer' out.txt &&\
+  ! grep 'uninitialized' out.txt &&\
+  ! grep 'incompatible pointer to' out.txt &&\
+  ! grep 'incompatible integer to' out.txt &&\
   ! grep 'type specifier missing' out.txt &&\
-  echo here3 &&\
-  ! $CC1 small.c -o small1 > cc_out1.txt 2>&1 &&\
-  grep $CRASHSTR cc_out1.txt &&\
-  echo here4 &&\
-  ~/c-semantics-bundle-0.1/c-semantics/dist/kcc -s small.c >/dev/null 2>&1 &&\
-  echo here5 
+  ! XXOPT1 small.c -o small1 > cc_out1.txt 2>&1 &&\
+  grep XXFAIL cc_out1.txt
 then
-  if 
-    ./a.out 
-  then
-    echo here6
-    exit 0
-  else
-    TMPF="undefined_$$.c"
-    echo copying C file to $TMPF
-    cp small.c $TMPF
-    exit 1
-  fi
+  exit 0
 else
   exit 1
 fi
+
+#  clang --analyze small.c > out_analyze.txt 2>&1 &&\
+#  ! grep garbage out_analyze.txt &&\
+#  ! grep undefined out_analyze.txt &&\
--- utah/scripts/reduce/test1_wrong_code.sh
+++ utah/scripts/reduce/test1_wrong_code.sh
@@ -1,64 +1,56 @@
 #!/bin/bash
 
-# todo: these come from environment variables
-
-CC1='gcc -w -O0'
-CC2='gcc -w -O3 -funroll-loops -fomit-frame-pointer -fwhole-program'
-
 rm -f out*.txt
 
 if 
-  echo here1 &&\
-  current-gcc -Wall -Wextra -O2 small.c  >outa.txt 2>&1 &&\
-  echo here2 &&\
+  echo here0 &&\
+  gcc -Wall -Wextra -O2 small.c -o smallz >outa.txt 2>&1 &&\
   ! grep uninitialized outa.txt &&\
   ! grep 'control reaches end' outa.txt &&\
   ! grep 'no semicolon at end' outa.txt &&\
   ! grep 'incompatible pointer' outa.txt &&\
   ! grep 'ordered comparison of pointer with integer' outa.txt &&\
   ! grep 'declaration does not declare anything' outa.txt &&\
+  ! grep 'arguments for format' outa.txt &&\
+  ! grep 'expects type' outa.txt &&\
   ! grep 'pointer from integer' outa.txt &&\
   ! grep 'incompatible implicit' outa.txt &&\
   ! grep 'excess elements in struct initializer' outa.txt &&\
   ! grep 'comparison between pointer and integer' outa.txt &&\
   clang -pedantic -Wall -O0 -c small.c  >out.txt 2>&1 &&\
+  ! grep 'conversions than data arguments' out.txt &&\
   ! grep 'incompatible redeclaration' out.txt &&\
   ! grep 'ordered comparison between pointer' out.txt &&\
   ! grep 'eliding middle term' out.txt &&\
   ! grep 'end of non-void function' out.txt &&\
   ! grep 'invalid in C99' out.txt &&\
+  ! grep 'specifies type' out.txt &&\
   ! grep 'should return a value' out.txt &&\
-  ! grep uninitialized out.txt &&\
+  ! grep 'uninitialized' out.txt &&\
   ! grep 'incompatible pointer to' out.txt &&\
   ! grep 'incompatible integer to' out.txt &&\
   ! grep 'type specifier missing' out.txt &&\
+  echo here1 &&\
   clang --analyze small.c > out_analyze.txt 2>&1 &&\
   ! grep garbage out_analyze.txt &&\
   ! grep undefined out_analyze.txt &&\
+  echo here2 &&\
+  RunSafely.sh 30 1 /dev/null out_vg.txt valgrind -q --error-exitcode=1 ./smallz &&\
   echo here3 &&\
-  $CC1 small.c -o small1 > cc_out1.txt 2>&1 &&\
-  RunSafely.sh 3 1 /dev/null out1.txt ./small1 >/dev/null 2>&1 &&\
-  valgrind -q --error-exitcode=1 ./small1 >/dev/null 2>&1 &&\
+  cp small.c small-framac.c &&\
+  perl -pi.bak -e 's/int main \(int argc, char\* argv\[\]\)/int argc; char **argv; int main (void)/' small-framac.c &&\
+  RunSafely.sh 125 1 /dev/null out_framac.txt frama-c -cpp-command \"gcc -C -Dvolatile= -E -I.\" -val-signed-overflow-alarms -val -stop-at-first-alarm -no-val-show-progress -machdep x86_64 -obviously-terminates -precise-unions small-framac.c &&\
+  ! grep assert out_framac.txt &&\
   echo here4 &&\
-  $CC2 small.c -o small2 > cc_out2.txt 2>&1 &&\
+  XXOPT1 small.c -o small1 > cc_out1.txt 2>&1 &&\
+  RunSafely.sh 3 1 /dev/null out1.txt ./small1 >/dev/null 2>&1 &&\
+  XXOPT2 small.c -o small2 > cc_out2.txt 2>&1 &&\
   RunSafely.sh 3 1 /dev/null out2.txt ./small2 >/dev/null 2>&1 &&\
-  ! diff out1.txt out2.txt &&\
-  echo here5 &&\
-  kcc -s small.c >/dev/null 2>&1 &&\
-  echo here6 
+  ! diff out1.txt out2.txt 
 then
-  if 
-    RunSafely.sh 1000 1 /dev/null out_kcc.txt ./a.out >/dev/null 2>&1
-  then
-    echo here7
-    exit 0
-  else
-    cat out_kcc.txt
-    TMPF="undefined_$$.c"
-    echo copying C file to $TMPF
-    cp small.c $TMPF
-    exit 1
-  fi
+  echo here5;
+  exit 0
 else
+  echo here6;
   exit 1
 fi
