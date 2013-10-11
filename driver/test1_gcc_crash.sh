6b96ab0cbc5d08f66f05cc202cd1cae06a8ad17d
John Regehr <regehr@cs.utah.edu>
2013-10-10 23:06:51 -0600

    decaying probability of auto-reduce with as we find the bug more times

--- driver/test1_gcc_crash.sh
+++ driver/test1_gcc_crash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ${CSMITH_HOME}/driver/check_unique.pl out.txt &&\
+
 ! XX_COMMAND small.c -w > out.txt 2>&1 &&\
-${CSMITH_HOME}/driver/check_unique.pl out.txt &&\
 grep XX_STRING out.txt
