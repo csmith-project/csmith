0dc1cd13294a820808c76c2c9e522550b7f68187
John Regehr <regehr@cs.utah.edu>
2013-10-10 23:46:41 -0600

    opportunstic crash discovery during reduction is coded and may work soon

--- driver/check_unique.pl
+++ driver/check_unique.pl
@@ -1,16 +1,44 @@
 #!/usr/bin/perl -w
 
 use strict;
+use File::Temp;
 
-res=$(grep -c 'compiler error' out.txt)
-if [ $res -gt 0 ]
-then
-    res=$(grep -c 'definition in block' out.txt)
-    if [ $res -eq 0 ]
-    then
-	FN=`hexdump -n 16 -v -e '/1 "%02X"' /dev/urandom`
-	cp small.c /mnt/local/randomtest/tmp/$FN
-    fi
-fi
+my $strings = $ARGV[0];
+die unless defined ($strings);
+my $crashfile = $ARGV[1];
+die unless defined ($crashfile);
+my $dir = $ARGV[2];
+die unless defined ($dir);
+
+my $err;
+open INF, "<$crashfile" or die;
+while (my $line = <INF>) {
+    chomp $line;
+    if ($line =~ /internal compiler error: (.*)$/) {
+	$err = $1;
+    }
+}
+close INF;
+exit 0 unless defined ($err);
+
+my $lines = "";
+my $found = 0;
+my $cnt;
+open INF, "<$strings" or die;
+while (my $line = <INF>) {
+    die unless ($line =~ /^([0-9]+) <<< (.*) >>>$/);
+    if ($2 eq $err) {
+	$found = 1;
+    }
+}
+close INF;
+
+if (!$found) {
+    open OUTF, ">>../crash_strings.txt" or die;
+    print OUTF "1 <<< $err >>>\n";
+    close OUTF;
+    my $tmpfn = File::Temp::tempnam ($dir, "crashXXXXXX");
+    system "cp small.c ${tmpfn}.c";
+}
 
 exit 0
--- driver/test1_gcc_crash.sh
+++ driver/test1_gcc_crash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ${CSMITH_HOME}/driver/check_unique.pl out.txt &&\
-
+gcc -w -c -O0 small.c &&\
 ! XX_COMMAND small.c -w > out.txt 2>&1 &&\
+${CSMITH_HOME}/driver/check_unique.pl XX_CRASHFILE out.txt XX_DIR &&\
 grep XX_STRING out.txt
