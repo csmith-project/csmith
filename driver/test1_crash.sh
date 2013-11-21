94db0ea336d6704ae0632ea21a3a48bb5c2d1958
John Regehr <regehr@cs.utah.edu>
2013-11-21 08:59:47 -0700

    avoid some obvious silly stuff in the crash interestingness test

--- driver/test1_crash.sh
+++ driver/test1_crash.sh
@@ -4,6 +4,10 @@ ulimit -t 120
 
 ${CSMITH_HOME}/driver/check_unique.pl XX_CRASHFILE XX_DIR XX_OPT "XX_COMMAND"
 
-gcc -w -c -O0 small.c &&\
+gcc -c small.c > gcc.out 2>&1 &&\
+! grep 'data definition has no type or storage class' gcc.out &&\
+! grep 'assumed to have one element' gcc.out &&\
+! grep 'control reaches end of non-void function' gcc.out &&\
+! grep 'return type defaults to' gcc.out &&\
 ! XX_COMMAND XX_OPT small.c -c -w > out.txt 2>&1 &&\
 grep XX_STRING out.txt
