b8378e92524eaedaf52d6f20c4dce9453adef2ca
John Regehr <regehr@cs.utah.edu>
2013-11-21 13:19:05 -0700

    cleanup reduction scripts preparing for blog post

--- driver/test1_wrong_code.sh
+++ driver/test1_wrong_code.sh
@@ -10,51 +10,51 @@
 
 rm -f out*.txt
 
-ulimit -t 120
+ulimit -t 600
 ulimit -v 8000000
 ulimit -m 8000000
 
 ${CSMITH_HOME}/driver/check_unique.pl XX_CRASHFILE XX_DIR XX_OPT "XX_COMMAND"
 
 if 
-  clang -pedantic -Wall -O0 -c small.c  >out.txt 2>&1 &&\
-  ! grep 'conversions than data arguments' out.txt &&\
-  ! grep 'incompatible redeclaration' out.txt &&\
-  ! grep 'ordered comparison between pointer' out.txt &&\
-  ! grep 'eliding middle term' out.txt &&\
-  ! grep 'end of non-void function' out.txt &&\
-  ! grep 'invalid in C99' out.txt &&\
-  ! grep 'specifies type' out.txt &&\
-  ! grep 'should return a value' out.txt &&\
-  ! grep 'uninitialized' out.txt &&\
-  ! grep 'incompatible pointer to' out.txt &&\
-  ! grep 'incompatible integer to' out.txt &&\
-  ! grep 'type specifier missing' out.txt &&\
-  gcc -Wall -Wextra -O1 small.c -o smallz >outa.txt 2>&1 &&\
-  ! grep uninitialized outa.txt &&\
-  ! grep 'without a cast' outa.txt &&\
-  ! grep 'control reaches end' outa.txt &&\
-  ! grep 'return type defaults' outa.txt &&\
-  ! grep 'cast from pointer to integer' outa.txt &&\
-  ! grep 'useless type name in empty declaration' outa.txt &&\
-  ! grep 'no semicolon at end' outa.txt &&\
-  ! grep 'type defaults to' outa.txt &&\
-  ! grep 'too few arguments for format' outa.txt &&\
-  ! grep 'incompatible pointer' outa.txt &&\
-  ! grep 'ordered comparison of pointer with integer' outa.txt &&\
-  ! grep 'declaration does not declare anything' outa.txt &&\
-  ! grep 'expects type' outa.txt &&\
-  ! grep 'pointer from integer' outa.txt &&\
-  ! grep 'incompatible implicit' outa.txt &&\
-  ! grep 'excess elements in struct initializer' outa.txt &&\
-  ! grep 'comparison between pointer and integer' outa.txt &&\
-  XX_COMMAND XX_OPT small.c -o small1 > cc_out1.txt 2>&1 &&\
-  ./small1 >out1.txt 2>&1 &&\
-  XX_COMMAND XX_GOOD small.c -o small2 > cc_out2.txt 2>&1 &&\
-  ./small2 >out2.txt 2>&1 &&\
-  ! diff out1.txt out2.txt &&\
-  # these options assume Frama-C Fluorine
-  RunSafely 125 1 /dev/null out_framac.txt frama-c -cpp-command \"gcc -C -Dvolatile= -E -I.\" -val -no-val-show-progress -machdep x86_64 -obviously-terminates small.c &&\
+  clang -pedantic -Wall -O0 -c small.c  >out_clang.txt 2>&1 &&\
+  ! grep 'conversions than data arguments' out_clang.txt &&\
+  ! grep 'incompatible redeclaration' out_clang.txt &&\
+  ! grep 'ordered comparison between pointer' out_clang.txt &&\
+  ! grep 'eliding middle term' out_clang.txt &&\
+  ! grep 'end of non-void function' out_clang.txt &&\
+  ! grep 'invalid in C99' out_clang.txt &&\
+  ! grep 'specifies type' out_clang.txt &&\
+  ! grep 'should return a value' out_clang.txt &&\
+  ! grep 'uninitialized' out_clang.txt &&\
+  ! grep 'incompatible pointer to' out_clang.txt &&\
+  ! grep 'incompatible integer to' out_clang.txt &&\
+  ! grep 'type specifier missing' out_clang.txt &&\
+  gcc -Wall -Wextra -O1 small.c -o smallz >out_gcc.txt 2>&1 &&\
+  ! grep 'uninitialized' out_gcc.txt &&\
+  ! grep 'without a cast' out_gcc.txt &&\
+  ! grep 'control reaches end' out_gcc.txt &&\
+  ! grep 'return type defaults' out_gcc.txt &&\
+  ! grep 'cast from pointer to integer' out_gcc.txt &&\
+  ! grep 'useless type name in empty declaration' out_gcc.txt &&\
+  ! grep 'no semicolon at end' out_gcc.txt &&\
+  ! grep 'type defaults to' out_gcc.txt &&\
+  ! grep 'too few arguments for format' out_gcc.txt &&\
+  ! grep 'incompatible pointer' out_gcc.txt &&\
+  ! grep 'ordered comparison of pointer with integer' out_gcc.txt &&\
+  ! grep 'declaration does not declare anything' out_gcc.txt &&\
+  ! grep 'expects type' out_gcc.txt &&\
+  ! grep 'pointer from integer' out_gcc.txt &&\
+  ! grep 'incompatible implicit' out_gcc.txt &&\
+  ! grep 'excess elements in struct initializer' out_gcc.txt &&\
+  ! grep 'comparison between pointer and integer' out_gcc.txt &&\
+  XX_COMMAND XX_OPT small.c -o small1 > /dev/null 2>&1 &&\
+  ./small1 >out_small1.txt 2>&1 &&\
+  XX_COMMAND XX_GOOD small.c -o small2 > /dev/null 2>&1 &&\
+  ./small2 >out_small2.txt 2>&1 &&\
+  ! diff out_small1.txt out_small2.txt &&\
+  # these options assume Frama-C Fluorine and a 64-bit machine
+  frama-c -cpp-command \"gcc -C -Dvolatile= -E -I.\" -val -no-val-show-progress -machdep x86_64 -obviously-terminates small.c > out_framac.txt 2>&1 &&\
   ! egrep -i '(user error|assert)' out_framac.txt >/dev/null 2>&1
 then
   exit 0
