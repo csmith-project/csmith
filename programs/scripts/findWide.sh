7e2d17c172f3e5a0c2cdbbf95bb48e6859155930
Jacek Burys <jsb314@ic.ac.uk>
2015-08-18 15:34:45 +0100

    scripts, findWide, findAllFloatVariables

---
+++ programs/scripts/findWide.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CSMITH="/home/jacek/Desktop/Imperial/UROP/csmith"
+ADAPTER="/home/jacek/Desktop/Imperial/UROP/Adapter/boost_interval_adapter/adaptertracking.o"
+TEMPDIR="temp"
+COMP="gcc"
+TIMEOUT="timeout 30"
+
+makeProg(){
+  $TIMEOUT ${CSMITH}/src/csmith --seed ${1} --strict-float --float-test > ${TEMPDIR}/prog.c
+  return $?
+}
+
+compileAndRun(){
+  $TIMEOUT gcc ${TEMPDIR}/prog.c -w -c -I${CSMITH}/runtime -DUNSAFE_FLOAT -DFLOAT_TEST_ENABLED -DTRACKING -DWIDE -o ${TEMPDIR}/prog1.o &&\
+  $TIMEOUT g++ ${TEMPDIR}/prog1.o $ADAPTER -o ${TEMPDIR}/prog1 &&\
+  $TIMEOUT ${TEMPDIR}/prog1 > ${TEMPDIR}/out1.txt
+  return $?
+}
+
+checkWide(){
+  $TIMEOUT grep 'Wide' ${TEMPDIR}/out1.txt
+  return $?
+}
+
+mkdir -p $TEMPDIR
+
+for i in $(seq $1 $2); do
+  makeProg $1 &&\
+  compileAndRun
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+  checkWide
+  if [ $? -eq 0 ]; then
+    echo "wide $i"
+  else
+    echo "singleton $i"
+  fi
+done
