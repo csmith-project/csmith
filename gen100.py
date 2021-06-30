7c0201a297acf4325ee6a965270b369d178e1e5b
jmanchuck <jjcheung0000@gmail.com>
2021-06-30 11:10:58 +0800

    Change seed to actual number string

--- gen100.py
+++ gen100.py
@@ -1,6 +1,5 @@
 import os
 
 for seed in range(100):
-    print("Generating with seed", seed) 
-    os.system("src/csmith -s seed > " + str(seed)+ ".c")
-
+    print("Generating with seed", seed)
+    os.system("src/csmith -s" + str(seed) + " > " + str(seed) + ".c")
