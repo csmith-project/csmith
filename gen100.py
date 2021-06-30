65c4b432d2afec7dfb6dd53fce66f4df2a9db984
Zhi Jun Cheung <50379115+jmanchuck@users.noreply.github.com>
2021-06-30 11:27:00 +0800

    Update left out a whitespace

--- gen100.py
+++ gen100.py
@@ -2,4 +2,4 @@ import os
 
 for seed in range(100):
     print("Generating with seed", seed)
-    os.system("src/csmith -s" + str(seed) + " > " + str(seed) + ".c")
+    os.system("src/csmith -s " + str(seed) + " > " + str(seed) + ".c")
