92aa4f2c4a782cefb340eadf9ecf6229db081177
John Regehr <john.regehr@gmail.com>
2026-02-27 13:10:46 -0700

    Revert "Update ArrayVariable.h (#156)"
    This reverts commit 03bc3e9cba6ecf145e31c283d2639f03b719137d.

--- src/ArrayVariable.h
+++ src/ArrayVariable.h
@@ -76,9 +76,9 @@ public:
 	virtual void OutputLowerBound(std::ostream &) const;
 	virtual void OutputUpperBound(std::ostream &) const;
 	void output_with_indices(std::ostream &out, const std::vector<const Variable*>& cvs) const;
-	void output_checksum_with_indices(std::ostream &out, const std::vector<const Variable*>& cvs, const string &field_name) const;
+	void output_checksum_with_indices(std::ostream &out, const std::vector<const Variable*>& cvs, string field_name) const;
 	void output_init(std::ostream &out, const Expression* init, const vector<const Variable*>& cvs, int indent) const;
-	void output_addr_checks(std::ostream &out, const Variable* var, const string &field_name, int indent) const;
+	void output_addr_checks(std::ostream &out, const Variable* var, string field_name, int indent) const;
 	void add_init_value(const Expression* e) { init_values.push_back(e);}
 	const vector<const Expression*>& get_init_values(void) const { return init_values;}
 	string build_initializer_str(const vector<string>& init_strings) const;
